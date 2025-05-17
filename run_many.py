a755683dc48bfa7eb16adb2d70b89aa7c367c0b1
leiyangjustin <yanglei.dalian@gmail.com>
2025-05-17 23:32:59 +0800

    run many shapes

--- run_many.py
+++ run_many.py
@@ -6,7 +6,7 @@ data_folder = '/home/lyang/yl_code/dataset/MPZ14/inputmodels/obj'
 shape_names = glob.glob(os.path.join(data_folder, '*.obj'))
 
 for i, shape_name in tqdm(enumerate(shape_names), desc='NGF Training'):
-    print(shape_name)
-    os.system(f'python source/train.py --mesh {shape_name} --lod 1000')
+    shape_name = os.path.splitext(os.path.basename(shape_name))[0]
+    os.system(f'python source/train.py --mesh {shape_name} --lod 2000')
     if i > 20:
         break
