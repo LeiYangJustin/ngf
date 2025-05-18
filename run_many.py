eafa9615e43a99d82c6291dc41560cea729a5235
leiyangjustin <yanglei.dalian@gmail.com>
2025-05-18 12:21:58 +0800

    update

--- run_many.py
+++ run_many.py
@@ -2,11 +2,26 @@ import os
 import glob
 from tqdm import tqdm
 
-data_folder = '/home/lyang/yl_code/dataset/MPZ14/inputmodels/obj'
-shape_names = glob.glob(os.path.join(data_folder, '*.obj'))
+data_folder = './data/MPZ14/inputmodels/obj'
+shape_paths = sorted(glob.glob(os.path.join(data_folder, '*.obj')))
 
-for i, shape_name in tqdm(enumerate(shape_names), desc='NGF Training'):
-    shape_name = os.path.splitext(os.path.basename(shape_name))[0]
-    os.system(f'python source/train.py --mesh {shape_name} --lod 2000')
-    if i > 20:
-        break
+wanted = [
+    "amphora",
+    "bimba100K",
+    "bunnyBotsch",
+    "Chinese_lion100K",
+    "bozbezbozzel50K",
+    "fat_dragon",   
+    "gargoyle100K",
+    "igea100k",
+    "rgb_dragon",
+    "dragonstand_recon100K"
+]
+wanted = [w.lower() for w in wanted]
+num_faces = 1000
+for i, shape_path in enumerate(shape_paths):
+
+    shape_name = os.path.splitext(os.path.basename(shape_path))[0]
+    if (shape_name.lower() in wanted):
+        print(shape_name)
+        os.system(f'python source/train.py --mesh {shape_path} --lod {num_faces}')
